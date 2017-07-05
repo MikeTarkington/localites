9e9af5473340c4acfdf6796d55ef67d92abcbd2e
Mike Tarkington <tarkmike@gmail.com>
2017-07-04 18:55:17 -0700

    rename key and token in env to be specific to yelp

--- app/models/YelpAdapter.rb
+++ app/models/YelpAdapter.rb
@@ -6,7 +6,7 @@ require 'dotenv/load'
 class YelpAdapter
 
   def initialize
-    @api_key = ENV["API_KEY"]
+    @api_key = ENV["YELP_API_KEY"]
   end
 
   def place_search(city, district, offset = 0)
@@ -17,7 +17,7 @@ class YelpAdapter
     request = Net::HTTP::Get.new(url)
     request["authorization"] = "Bearer #{@api_key}"
     request["cache-control"] = 'no-cache'
-    request["postman-token"] = ENV["POSTMAN_TOKEN"]
+    request["postman-token"] = ENV["YELP_POSTMAN_TOKEN"]
     response = http.request(request)
     @data_set = response.read_body
   end
@@ -34,5 +34,5 @@ class YelpAdapter
   # end
 
 end
-test = YelpAdapter.new
-p test.place_search('san francisco', 'mission')
+# test = YelpAdapter.new
+# p test.place_search('san francisco', 'mission')
