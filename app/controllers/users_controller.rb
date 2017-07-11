0dbc593318d6ca650c07312526375275e5f9b0ba
Mike Tarkington <tarkmike@gmail.com>
2017-07-10 19:47:35 -0700

    fill in custom route for find_by_firebase which returns the user object as json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    p "8" * 80
-    p params
     @user = User.find(params[:id])
     @users_plans = @user.plans
     # if @users_plans.count == 0
     #   @users_plans = "You currently do not have any preferred locations. Please select \"create plan\" form the menu above."
     # end
     render json: @users_plans
+  end
+
+  def find_by_firebase
+    @user = User.find_by(params[:firebase_uid])
+    render json: @user
   end
 
 end
